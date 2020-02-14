efb1ba442fe0f46bf125a1d68658267ff358d7c1
MurdocAV <azaan.virk1@gmail.com>
2020-02-15 11:31:35 +1300

    div working animation, hello world

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import anime from 'animejs/lib/anime.es.js';
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+anime({
+  targets: 'div',
+  translateX: 250,
+  rotate: '1turn',
+  backgroundColor: '#FFF',
+  duration: 800
+});
+
+class App extends React.Component  {
+  
+  render() {
+    return (
+      <React.Fragment>
+        <div className="animeDiv">
+          Hello World Div -> Let's animate.
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
