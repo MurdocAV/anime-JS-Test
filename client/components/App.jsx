2fb0eb53ed47310fe3e28601890dfef62bb39e25
MurdocAV <azaan.virk1@gmail.com>
2020-02-15 12:03:07 +1300

    Using button, non functional for state change

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -10,17 +10,36 @@ import anime from 'animejs/lib/anime.es.js';
 // });
 
 class App extends React.Component  {
-  
+  constructor () {
+    super ()
+
+    this.state = {
+      example: 'Hello World'
+    }
+  }
+
+  clickHandler = (evt) => {
+    evt.preventDefault()
+    this.setStat({
+      example: 'You changed the Hello World.'
+    })
+  }
+
+
   render() {
     return (
       <React.Fragment>
         <div className="wrapperDiv">
-            <button>Click me!</button>
+            <button className="activateAnime"
+              onClick={this.clickHandler}
+            >Click me!</button>
           <div className="innerDiv">
             <div className="animationDiv">
             </div>
           </div>
         </div>
+        <br/>
+        <p>{this.state.example}</p>
       </React.Fragment>
     )
   }
--- server/public/bundle.js
+++ server/public/bundle.js
@@ -108,13 +108,15 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 
  // anime({
@@ -131,9 +133,24 @@ function (_React$Component) {
   _inherits(App, _React$Component);
 
   function App() {
+    var _this;
+
     _classCallCheck(this, App);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(App).apply(this, arguments));
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(App).call(this));
+
+    _defineProperty(_assertThisInitialized(_this), "clickHandler", function (evt) {
+      evt.preventDefault();
+
+      _this.setStat({
+        example: 'You changed the Hello World.'
+      });
+    });
+
+    _this.state = {
+      example: 'Hello World'
+    };
+    return _this;
   }
 
   _createClass(App, [{
@@ -141,11 +158,14 @@ function (_React$Component) {
     value: function render() {
       return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "wrapperDiv"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", null, "Click me!"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        className: "activateAnime",
+        onClick: this.clickHandler
+      }, "Click me!"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "innerDiv"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "animationDiv"
-      }))));
+      }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, this.state.example));
     }
   }]);
 
